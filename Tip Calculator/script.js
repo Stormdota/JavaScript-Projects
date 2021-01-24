6d9bb7826f238e49426fc7e2d795d653ba3e2c58
Storm <joshihari4@gmail.com>
2021-01-24 13:25:38 +0530

    1st version

--- Tip Calculator/script.js
+++ Tip Calculator/script.js
@@ -1,1 +1,40 @@
-console.log("Script is working");
+//console.log("Script is working");
+
+function calculatetips(){
+    // Get the values entered by the users
+    var amt = document.getElementById("amt").value;
+    var persons = document.getElementById("pers").value;
+    var service = document.getElementById("service").value;  
+    
+    // Validate the input by the user
+
+    if(amt === "" || amt < 0){
+        alert("Enter the correct amount value");
+        return;
+    }
+     if( persons === "" || persons < 0){
+        alert("Enter the number of person sharing the bill");
+        return;
+    }
+
+    // Validate the type of service
+
+    if(service == 0){
+        alert("Please select the type of service");
+        return;
+    }
+    // Calculating the tip
+
+    var tips = (amt*service)/persons;
+   
+    // Displaying the tip amount per person
+
+    var tipp = document.createElement("p");
+    tipp.innerText = "The tip for each person is: " + tips;
+    document.getElementById("tip").appendChild(tipp);
+
+
+   
+
+
+}
